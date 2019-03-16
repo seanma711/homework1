9026fd8fa9156ec46fb1eab92dd58ec228c44301
Sean Ma <logiclegolas@gmail.com>
2019-03-16 12:21:34 -0400

    Made Rails calculator
    Converted the calculator to Ruby on Rails from JavaScript

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -1,7 +1,37 @@
 class CalculatorController < ApplicationController
   def convert
-    @x = params[:x]
-    @a = params[:a]
-    @b = params[:b]
+    @input = params[:x].to_f
+    @origin_unit = params[:a]
+    @convert_unit = params[:b]
+    @output
+    @kelvin_difference = 273.15
+    if @origin_unit === "k"
+      if @convert_unit === 'f'
+        @output = (@input - 273.15)*(9.0/5.0)+32.0
+      elsif @convert_unit ==='c'
+        @output = (@input - 273.15)
+      else
+        @output='Unable to convert'
+      end
+    elsif @origin_unit ==='c'
+      if @convert_unit === 'f'
+        @output = @input*(9.0/5.0)+32.0
+      elsif @convert_unit === 'k'
+        @output = @input + 273.15
+      else
+        @output='Unable to convert'
+      end
+    elsif @origin_unit ==='f'
+      if @convert_unit === 'c'
+        @output = @input*(5.0/9.0)-32.0
+      elsif @convert_unit === 'k'
+        @output = (@input-32.0)*(5.0/9.0)+273.15
+      else
+        @output='Unable to convert'
+      end
+    else
+      @output='Invalid initial unit'
+    end
+    @output = @output.round(1)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  get "/calculator/:x/:a/to/:b", to: "calculator#convert"
+  get '/calculator/:x/:a/to/:b', to: 'calculator#convert'
 end
